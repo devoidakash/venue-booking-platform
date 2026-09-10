1c09657a43939164cf05f8a80260ac58d7603d71
akpatel-1 <akpatel.work1@gmail.com>
2026-09-10 14:31:38 +0530

    feat: implement axios interceptors for token refresh handling and add admin axios instance

--- client/src/lib/axios.instance.js
+++ client/src/lib/axios.instance.js
@@ -5,8 +5,59 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.response.use((response) => {
-  return response.data.data;
-});
+let isRefreshing = false;
+let refreshSubscribers = [];
+
+function subscribeTokenRefresh(callback) {
+  refreshSubscribers.push(callback);
+}
+
+function onRefreshed() {
+  refreshSubscribers.forEach((callback) => callback());
+  refreshSubscribers = [];
+}
+
+axiosInstance.interceptors.response.use(
+  (response) => response.data.data,
+
+  async (error) => {
+    const { response, config } = error;
+
+    if (!response) return Promise.reject(error);
+
+    const code = response.data?.code;
+
+    if (code === "SESSION_EXPIRED") {
+      window.location.href = "/login";
+      return Promise.reject(error);
+    }
+
+    if (code === "ACCESS_TOKEN_MISSING" && !config._retry) {
+      config._retry = true;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          await axiosInstance.post("/auth/refresh");
+          isRefreshing = false;
+          onRefreshed();
+        } catch (refreshError) {
+          isRefreshing = false;
+          refreshSubscribers = [];
+          window.location.href = "/login";
+          return Promise.reject(refreshError);
+        }
+      }
+
+      return new Promise((resolve) => {
+        subscribeTokenRefresh(() => {
+          resolve(axiosInstance(config));
+        });
+      });
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
