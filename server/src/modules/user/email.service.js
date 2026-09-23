745cfa54535360e0a0b942dd1c2798e11282da44
akpatel-1 <akpatel.work1@gmail.com>
2026-09-23 11:09:21 +0530

    refactor: relocate and enhance OTP email service for improved structure and error handling

--- server/src/modules/user/email.service.js
+++ server/src/modules/user/email.service.js
@@ -1,15 +1,33 @@
+import { ERROR_CONFIG } from '../../config/error.config.js';
 import resend from '../../infrastructure/email/email.js';
+import { generateOtpTemplate } from '../../infrastructure/email/templates/otp.template.js';
 import { generateBookingConfirmationTemplate } from '../../infrastructure/email/templates/venue/booking.confirmation.js';
+import ApiError from '../../utils/api.error.js';
 import { generateBookingQR } from '../../utils/generateqr.js';
-import * as repository from './venue/booking/repository.js';
 
 const FROM = 'Venuz <noreply@venuz.shop>';
+
+export default async function sendOtpEmail(email, otp) {
+  try {
+    const response = await resend.emails.send({
+      from: FROM,
+      to: email,
+      subject: 'Your Venuz Verification Code',
+      html: generateOtpTemplate(otp),
+    });
+
+    console.log(otp);
+    return response;
+  } catch (error) {
+    throw new ApiError(ERROR_CONFIG.EMAIL_SEND_FAILED);
+  }
+}
 
 export async function sendBookingConfirmationEmail({
   email,
   bookingData,
   venue,
-} = {}) {
+}) {
   const qrDataUrl = await generateBookingQR(bookingData.id);
   const qrContent = qrDataUrl.replace(/^data:image\/png;base64,/, '');
 
