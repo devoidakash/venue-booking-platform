9f0c84a404e28d5f4ac8f610c67f25527dfd76b4
akpatel-1 <akpatel.work1@gmail.com>
2026-09-24 07:22:10 +0530

    fix: handle null input in toCamelCase function

--- server/src/utils/camelcase.conversion.js
+++ server/src/utils/camelcase.conversion.js
@@ -1,4 +1,6 @@
 export default function toCamelCase(row) {
+  if (row == null) return null;
+
   return Object.fromEntries(
     Object.entries(row).map(([key, value]) => [
       key.replace(/_([a-z])/g, (_, char) => char.toUpperCase()),
